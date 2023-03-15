7b29227956a79f4b45f71a12848111b75c3a6359
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2023-03-15 21:56:00 +0530

    Modified form exit dialog UI. Added "save_by_default" setting to save forms by default on exit.

--- odk/collect/collect_app/src/main/java/org/odk/collect/android/formentry/QuitFormDialogFragment.java
+++ odk/collect/collect_app/src/main/java/org/odk/collect/android/formentry/QuitFormDialogFragment.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,24 +14,18 @@ import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.common.collect.ImmutableList;
 
 import org.odk.collect.analytics.Analytics;
 import org.odk.collect.android.R;
-import org.odk.collect.android.adapters.IconMenuListAdapter;
-import org.odk.collect.android.adapters.model.IconMenuItem;
 import org.odk.collect.android.external.InstancesContract;
 import org.odk.collect.android.formentry.saving.FormSaveViewModel;
 import org.odk.collect.android.injection.DaggerUtils;
 import org.odk.collect.android.projects.CurrentProjectProvider;
-import org.odk.collect.android.utilities.DialogUtils;
 import org.odk.collect.android.utilities.InstancesRepositoryProvider;
 import org.odk.collect.async.Scheduler;
 import org.odk.collect.forms.instances.Instance;
 import org.odk.collect.settings.SettingsProvider;
 import org.odk.collect.settings.keys.ProtectedProjectKeys;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -84,26 +77,57 @@ public class QuitFormDialogFragment extends DialogFragment {
 
         String title = formSaveViewModel.getFormName() == null ? getActivity().getString(R.string.no_form_loaded) : formSaveViewModel.getFormName();
 
-        List<IconMenuItem> items;
+        // WARNING: Custom ODK Changes
+        MaterialAlertDialogBuilder formExitDialogBuilder = new MaterialAlertDialogBuilder(getActivity())
+                .setTitle(getString(R.string.quit_application, title))
+                .setNegativeButton(getActivity().getString(R.string.do_not_exit), (dialog, id) -> {
+                    dialog.cancel();
+                    dismiss();
+                });
+
         if (settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_SAVE_MID)) {
-            items = ImmutableList.of(new IconMenuItem(R.drawable.ic_save, R.string.keep_changes),
-                    new IconMenuItem(R.drawable.ic_delete, R.string.do_not_save));
-        } else {
-            items = ImmutableList.of(new IconMenuItem(R.drawable.ic_delete, R.string.do_not_save));
-        }
+            if (settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_SAVE_BY_DEFAULT)) {
+                formExitDialogBuilder.setMessage(R.string.confirm_exit_with_save);
+            }
+            else {
+                formExitDialogBuilder.setMessage(R.string.confirm_exit);
+                formExitDialogBuilder.setNeutralButton(getActivity().getString(R.string.exit), (dialog, id) -> {
+                    formSaveViewModel.ignoreChanges();
+                    formEntryViewModel.exit();
 
-        ListView listView = DialogUtils.createActionListView(getActivity());
-
-        final IconMenuListAdapter adapter = new IconMenuListAdapter(getActivity(), items);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            IconMenuItem item = (IconMenuItem) adapter.getItem(position);
-
-            if (item.getTextResId() == R.string.keep_changes) {
+                    String action = getActivity().getIntent().getAction();
+                    if (Intent.ACTION_PICK.equals(action) || Intent.ACTION_EDIT.equals(action)) {
+                        // caller is waiting on a picked form
+                        Uri uri = null;
+                        String path = formSaveViewModel.getAbsoluteInstancePath();
+                        if (path != null) {
+                            Instance instance = new InstancesRepositoryProvider(requireContext()).get().getOneByPath(path);
+                            if (instance != null) {
+                                uri = InstancesContract.getUri(currentProjectProvider.getCurrentProject().getUuid(), instance.getDbId());
+                            }
+                        }
+                        if (uri != null) {
+                            getActivity().setResult(RESULT_OK, new Intent().setData(uri));
+                        }
+                    }
+                    getActivity().finish();
+                    if (getDialog() != null) {
+                        getDialog().dismiss();
+                    }
+                });
+            }
+            formExitDialogBuilder.setPositiveButton(getActivity().getString(R.string.keep_changes), (dialog, id) -> {
                 if (listener != null) {
                     listener.onSaveChangesClicked();
                 }
-            } else {
+                if (getDialog() != null) {
+                    getDialog().dismiss();
+                }
+            });
+        }
+        else {
+            formExitDialogBuilder.setMessage(R.string.confirm_exit_without_save);
+            formExitDialogBuilder.setPositiveButton(getActivity().getString(R.string.exit), (dialog, id) -> {
                 formSaveViewModel.ignoreChanges();
                 formEntryViewModel.exit();
 
@@ -118,27 +142,17 @@ public class QuitFormDialogFragment extends DialogFragment {
                             uri = InstancesContract.getUri(currentProjectProvider.getCurrentProject().getUuid(), instance.getDbId());
                         }
                     }
-
                     if (uri != null) {
                         getActivity().setResult(RESULT_OK, new Intent().setData(uri));
                     }
                 }
                 getActivity().finish();
-            }
-
-            if (getDialog() != null) {
-                getDialog().dismiss();
-            }
-        });
-
-        return new MaterialAlertDialogBuilder(getActivity())
-                .setTitle(getString(R.string.quit_application, title))
-                .setNegativeButton(getActivity().getString(R.string.do_not_exit), (dialog, id) -> {
-                    dialog.cancel();
-                    dismiss();
-                })
-                .setView(listView)
-                .create();
+                if (getDialog() != null) {
+                    getDialog().dismiss();
+                }
+            });
+        }
+        return formExitDialogBuilder.create();
     }
 
     public interface Listener {
