cd6e76522eecb8198c2bdc0592de10cf58fbc162
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2023-03-15 21:12:05 +0530

    Resolve APPLICATION_ID dependency

--- odk/collect/collect_app/src/main/java/org/odk/collect/android/utilities/ThemeUtils.java
+++ odk/collect/collect_app/src/main/java/org/odk/collect/android/utilities/ThemeUtils.java
@@ -111,7 +111,9 @@ public final class ThemeUtils {
     }
 
     private String getPrefsTheme() {
-        return settingsProvider.getUnprotectedSettings().getString(ProjectKeys.KEY_APP_THEME);
+        String theme = settingsProvider.getUnprotectedSettings().getString(ProjectKeys.KEY_APP_THEME);
+        if (theme != null) return theme;
+        else return context.getResources().getString(R.string.app_theme_light);
     }
 
     /**
